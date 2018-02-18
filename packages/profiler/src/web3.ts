248786482d044277213a3280ea2bf49da5450a28
Leonid Logvinov <logvinov.leon@gmail.com>
2018-02-18 10:46:18 -0700

    Revert "Change url"
    This reverts commit 3460a7e18ff873709429db14a6536b6bccb9d14c.

--- packages/profiler/src/web3.ts
+++ packages/profiler/src/web3.ts
@@ -1,5 +1,5 @@
 import { Web3Wrapper } from '@0xproject/web3-wrapper';
 import * as Web3 from 'web3';
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.0xproject.com'));
+const web3 = new Web3(new Web3.providers.HttpProvider('http://node.web3api.com:8545'));
 export const web3Wrapper = new Web3Wrapper(web3.currentProvider);
